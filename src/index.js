c747abae94a7091c7bee734fba1384588b25d347
christian-gerard <1@christiangerard.me>
2024-02-09 10:57:59 -0700

    Adds url variables to change photo query

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    console.log('WORKING!!!')
+    const url = 'https://picsum.photos'
+    const photoWidth = 600
+    const photoHeight = 400
+    const grayscale = true
+    const blur = 2
+
+
+    fetch(`${url}/${photoWidth}/${photoHeight}/?${grayscale ? 'grayscale&' : ''}${blur ? 'blur=' + blur : ''}`)
+    .then(resp => {
+        const imgUrl = resp['url']
+        const randomPhoto = document.querySelector('#display-photo')
+
+        randomPhoto.src = imgUrl
+        console.log(resp)
+
+
+        
+    })
+    
 })
 
