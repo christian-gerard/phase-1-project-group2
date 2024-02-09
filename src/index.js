f1a2a10f25f73abe1f185216cf68fe9336d4b037
christian-gerard <1@christiangerard.me>
2024-02-09 11:41:00 -0700

    Adds loading display functions

--- src/index.js
+++ src/index.js
@@ -2,22 +2,62 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const url = 'https://picsum.photos'
     const photoWidth = 600
-    const photoHeight = 400
+    const photoHeight = 800
     const grayscale = true
-    const blur = 2
+    const blur = 5
+
+    const randomPhoto = document.querySelector('#display-photo')
+    const photoContainer = 'Select parent Div of the random image once this code has been merged'
 
 
-    fetch(`${url}/${photoWidth}/${photoHeight}/?${grayscale ? 'grayscale&' : ''}${blur ? 'blur=' + blur : ''}`)
-    .then(resp => {
-        const imgUrl = resp['url']
-        const randomPhoto = document.querySelector('#display-photo')
 
-        randomPhoto.src = imgUrl
-        console.log(resp)
-
-
+    const fetchPhoto = () => {
         
-    })
+        displayLoading(randomPhoto)
+
+        fetch(`${url}/${photoWidth}/${photoHeight}/?${grayscale ? 'grayscale&' : ''}${blur ? 'blur=' + blur : ''}`)
+        .then(resp => {
+            const imgUrl = resp['url']
+            
+            hideLoading()
+            randomPhoto.src = imgUrl
+            
+        })
+
+    }
+
+    const displayLoading = (element) => {
+        console.log('Display Loading')
+        element.alt = 'random-photo'
+    }
+
+    const hideLoading = () => {
+
+        console.log('Hide Loading')
+    }
+
+    const main = () => {
+        //Get the photo from lorem picsum
+        fetchPhoto()
+
+
+    }
+
+
+    main()
     
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
 
