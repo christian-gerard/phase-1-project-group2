3e6740f174b073ebbe58b95a427866d1cd2d4df0
Kia Neitzell <kia.neitzell@gmail.com>
2024-02-09 15:27:10 -0800

    addEventListener1

--- src/index.js
+++ src/index.js
@@ -8,47 +8,57 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const randomPhoto = document.querySelector('#display-photo')
     const photoMessage = document.querySelector('#photo-message')
+    const blackAndWhite = document.querySelector('#toggle')
+    //! kias test
+    const renderPhoto = () => {
 
-//! kias test
+    }
+    blackAndWhite.addEventListener('change', (event) => {
+        const btn = event.target.value
+        if (btn === 'on') { // Corrected the comparison operator from "=" to "==="
+            console.log('event worked');
+        }
+    });
+})
 
-    const fetchPhoto = () => {
-        
-        displayLoading()
+const fetchPhoto = () => {
 
-        fetch(`${url}/${photoWidth}/${photoHeight}/?${grayscale ? 'grayscale&' : ''}${blur ? 'blur=' + blur : ''}`)
+    displayLoading()
+
+    fetch(`${url}/${photoWidth}/${photoHeight}/?${grayscale ? 'grayscale&' : ''}${blur ? 'blur=' + blur : ''}`)
         .then(resp => {
             const imgUrl = resp['url']
-            
+
             hideLoading()
             randomPhoto.src = imgUrl
-            
+
         })
         .catch(err => console.log(err))
 
-    }
+}
 
-    const displayLoading = () => {
-        photoMessage.classList.add('display')
-
-       
-        
-    }
-
-    const hideLoading = () => {
-
-        photoMessage.classList.remove('display')
-    }
-
-    const main = () => {
-        //Get the photo from lorem picsum
-        fetchPhoto()
+const displayLoading = () => {
+    photoMessage.classList.add('display')
 
 
-    }
+
+}
+
+const hideLoading = () => {
+
+    photoMessage.classList.remove('display')
+}
+
+const main = () => {
+    //Get the photo from lorem picsum
+    fetchPhoto()
 
 
-    main()
-    
+}
+
+
+main()
+
 
 
 })
